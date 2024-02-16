ab39a20c965e55f8fba92e2ff7a5313e26875c94
Ricardo Alf <rear_recinos@hotmail.com>
2024-02-15 19:03:52 -0600

    Change styles teeth form

--- src/app/private/page.tsx
+++ src/app/private/page.tsx
@@ -9,101 +9,10 @@ import HeadComponent from '@/components/HeadComponent'
 import { SlArrowDown } from 'react-icons/sl'
 import InputDate from '@/components/InputDate'
 import { Accordion, AccordionDetails, AccordionSummary, Button } from '@mui/material'
+import { FiSave, FiXCircle } from 'react-icons/fi'
 import '@/styles/private.css'
 
-import Box from '@mui/material/Box'
-import Tab from '@mui/material/Tab'
-import TabContext from '@mui/lab/TabContext'
-import TabList from '@mui/lab/TabList'
-import TabPanel from '@mui/lab/TabPanel'
-import { FiSave, FiXCircle } from 'react-icons/fi'
-
 function Home() {
-	const teeth = [
-		[
-			[
-				{ title: '18', value: 18 },
-				{ title: '17', value: 17 },
-				{ title: '16', value: 16 },
-				{ title: '15', value: 15 },
-				{ title: '14', value: 14 },
-				{ title: '13', value: 13 },
-				{ title: '12', value: 12 },
-				{ title: '11', value: 11 },
-			],
-
-			[
-				{ title: '21', value: 21 },
-				{ title: '22', value: 22 },
-				{ title: '23', value: 23 },
-				{ title: '24', value: 24 },
-				{ title: '25', value: 25 },
-				{ title: '26', value: 26 },
-				{ title: '27', value: 27 },
-				{ title: '28', value: 28 },
-			],
-		],
-
-		[
-			[
-				{ title: '55', value: 55 },
-				{ title: '54', value: 54 },
-				{ title: '53', value: 53 },
-				{ title: '52', value: 52 },
-				{ title: '51', value: 51 },
-			],
-
-			[
-				{ title: '61', value: 61 },
-				{ title: '62', value: 62 },
-				{ title: '63', value: 63 },
-				{ title: '64', value: 64 },
-				{ title: '65', value: 65 },
-			],
-		],
-
-		[
-			[
-				{ title: '85', value: 85 },
-				{ title: '84', value: 84 },
-				{ title: '83', value: 83 },
-				{ title: '82', value: 82 },
-				{ title: '81', value: 81 },
-			],
-
-			[
-				{ title: '71', value: 71 },
-				{ title: '72', value: 72 },
-				{ title: '73', value: 73 },
-				{ title: '74', value: 74 },
-				{ title: '75', value: 75 },
-			],
-		],
-
-		[
-			[
-				{ title: '48', value: 48 },
-				{ title: '47', value: 47 },
-				{ title: '46', value: 46 },
-				{ title: '45', value: 45 },
-				{ title: '44', value: 44 },
-				{ title: '43', value: 43 },
-				{ title: '42', value: 42 },
-				{ title: '41', value: 41 },
-			],
-			[
-				{ title: '31', value: 31 },
-				{ title: '32', value: 32 },
-				{ title: '33', value: 33 },
-				{ title: '34', value: 34 },
-				{ title: '35', value: 35 },
-				{ title: '36', value: 36 },
-				{ title: '37', value: 37 },
-				{ title: '38', value: 38 },
-			],
-		],
-	]
-
 	const [tooth, setTooth] = useState<number>(0)
 
 	const [tabContext, setTabContext] = useState<string>('1')
@@ -417,70 +326,9 @@ function Home() {
 							</div>
 						</AccordionDetails>
 					</Accordion>
-					<div></div>
-					<TabContext value={tabContext}>
-						<Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-							<TabList aria-label="Formulario de dientes">
-								<Tab label="Lista de dientes" value="1" />
-								<Tab label="Estado del diente" value="2" />
-								{tooth !== 0 && (
-									<Tab label={`Numero de diente = ${tooth}`} value={tooth} />
-								)}
-							</TabList>
-						</Box>
-						<TabPanel value="1">
-							<div className="teethFirstRow">
-								{teeth.map((value, i) => {
-									return (
-										<div className="teethFirstRow_row" key={`value${i}`}>
-											{value.map((number, i) => {
-												return (
-													<div className="row" key={`row${i}`}>
-														{number.map((num, i) => {
-															return (
-																<Button
-																	type="button"
-																	variant={
-																		tooth === num.value
-																			? 'contained'
-																			: 'outlined'
-																	}
-																	key={`button${i}`}
-																	onClick={() =>
-																		handleToothNumber(num.value)
-																	}
-																>
-																	{num.title}
-																</Button>
-															)
-														})}
-													</div>
-												)
-											})}
-										</div>
-									)
-								})}
-							</div>
-						</TabPanel>
-						<TabPanel value="2">
-							<ToothForm
-								tooth={tooth}
-								stateTooth={stateTooth}
-								palatina={palatina}
-								distal={distal}
-								mesial={mesial}
-								vestibular={vestibular}
-								oclusal={oclusal}
-								disable={tooth === 0}
-								handleSaveStates={handleSaveStates}
-								statePositionTooth={statePositionTooth}
-								handleDeleteStates={handleDeleteStates}
-								handleToothPosition={handleToothPosition}
-								handleFormControlStatePosition={handleFormControlStatePosition}
-								handleFormControlToothStateType={handleFormControlToothStateType}
-							/>
-						</TabPanel>
-					</TabContext>
+
+					{/* <ToothForm  /> */}
+
 					<div className="btn_group">
 						<Button
 							variant="contained"
--- src/components/teeth/ToothCheckbox.tsx
+++ src/components/teeth/ToothCheckbox.tsx
@@ -29,97 +29,134 @@ interface ToothFormProps {
 }
 
 const ToothForm = (props: ToothFormProps) => {
+	const teeth = [
+		[
+			[
+				{ title: '18', value: 18 },
+				{ title: '17', value: 17 },
+				{ title: '16', value: 16 },
+				{ title: '15', value: 15 },
+				{ title: '14', value: 14 },
+				{ title: '13', value: 13 },
+				{ title: '12', value: 12 },
+				{ title: '11', value: 11 },
+			],
+
+			[
+				{ title: '21', value: 21 },
+				{ title: '22', value: 22 },
+				{ title: '23', value: 23 },
+				{ title: '24', value: 24 },
+				{ title: '25', value: 25 },
+				{ title: '26', value: 26 },
+				{ title: '27', value: 27 },
+				{ title: '28', value: 28 },
+			],
+		],
+
+		[
+			[
+				{ title: '55', value: 55 },
+				{ title: '54', value: 54 },
+				{ title: '53', value: 53 },
+				{ title: '52', value: 52 },
+				{ title: '51', value: 51 },
+			],
+
+			[
+				{ title: '61', value: 61 },
+				{ title: '62', value: 62 },
+				{ title: '63', value: 63 },
+				{ title: '64', value: 64 },
+				{ title: '65', value: 65 },
+			],
+		],
+
+		[
+			[
+				{ title: '85', value: 85 },
+				{ title: '84', value: 84 },
+				{ title: '83', value: 83 },
+				{ title: '82', value: 82 },
+				{ title: '81', value: 81 },
+			],
+
+			[
+				{ title: '71', value: 71 },
+				{ title: '72', value: 72 },
+				{ title: '73', value: 73 },
+				{ title: '74', value: 74 },
+				{ title: '75', value: 75 },
+			],
+		],
+
+		[
+			[
+				{ title: '48', value: 48 },
+				{ title: '47', value: 47 },
+				{ title: '46', value: 46 },
+				{ title: '45', value: 45 },
+				{ title: '44', value: 44 },
+				{ title: '43', value: 43 },
+				{ title: '42', value: 42 },
+				{ title: '41', value: 41 },
+			],
+			[
+				{ title: '31', value: 31 },
+				{ title: '32', value: 32 },
+				{ title: '33', value: 33 },
+				{ title: '34', value: 34 },
+				{ title: '35', value: 35 },
+				{ title: '36', value: 36 },
+				{ title: '37', value: 37 },
+				{ title: '38', value: 38 },
+			],
+		],
+	]
+
 	return (
-		<div className={styles.toothForm}>
-			<div className={styles.toothFormStates}>
-				<h2>El número de diente es: {props.tooth}</h2>
-				<FormControl className={styles.formControl}>
-					<FormLabel id="estado-del-diente">Estado del diente</FormLabel>
-					<RadioGroup
-						row
-						aria-labelledby="estado-del-diente"
-						defaultValue=""
-						name="statePositionTooth"
-						className={styles.radioGroup}
-						value={props.statePositionTooth}
-						onChange={props.handleFormControlStatePosition}
-					>
-						<FormControlLabel value="decay" control={<Radio />} label="Carie" />
-						<FormControlLabel value="filling" control={<Radio />} label="Relleno" />
-						<FormControlLabel value="" control={<Radio />} label="Blanco" />
-					</RadioGroup>
-				</FormControl>
-				<FormControl className={styles.formControl}>
-					<FormLabel id="estado-de-extracción">Estado de extracción</FormLabel>
-					<RadioGroup
-						row
-						aria-labelledby="estado-de-extracción"
-						defaultValue=""
-						name="toothStateType"
-						value={props.stateTooth}
-						onChange={props.handleFormControlToothStateType}
-						className={styles.radioGroup}
-					>
-						<FormControlLabel
-							value="extraction"
-							control={<Radio />}
-							label="A extracción"
-						/>
-						<FormControlLabel value="extracted" control={<Radio />} label="Extraida" />
-						<FormControlLabel value="" control={<Radio />} label="Blanco" />
-					</RadioGroup>
-				</FormControl>
-				<div className={styles.btnGroup}>
-					<IconButton aria-label="save" onClick={props.handleSaveStates}>
-						<FiSave size={24} />
-					</IconButton>
-					<IconButton aria-label="fix circle" onClick={props.handleDeleteStates}>
-						<FiXCircle size={24} />
-					</IconButton>
-				</div>
-			</div>
-			<div className={styles.toothFormTooth}>
-				<Button
-					className={styles.palatina}
-					variant={props.palatina === '' ? 'outlined' : 'contained'}
-					color={props.palatina === 'decay' ? 'error' : 'info'}
-					onClick={() => props.handleToothPosition('palatina')}
-				>
-					Palatina
-				</Button>
-				<Button
-					className={styles.mesial}
-					variant={props.mesial === '' ? 'outlined' : 'contained'}
-					color={props.mesial === 'decay' ? 'error' : 'info'}
-					onClick={() => props.handleToothPosition('mesial')}
-				>
-					Mesial
-				</Button>
-				<Button
-					className={styles.distal}
-					variant={props.distal === '' ? 'outlined' : 'contained'}
-					color={props.distal === 'decay' ? 'error' : 'info'}
-					onClick={() => props.handleToothPosition('distal')}
-				>
-					Distal
-				</Button>
-				<Button
-					className={styles.vestibular}
-					variant={props.vestibular === '' ? 'outlined' : 'contained'}
-					color={props.vestibular === 'decay' ? 'error' : 'info'}
-					onClick={() => props.handleToothPosition('vestibular')}
-				>
-					Vestibular
-				</Button>
-				<Button
-					className={styles.oclusal}
-					variant={props.oclusal === '' ? 'outlined' : 'contained'}
-					color={props.oclusal === 'decay' ? 'error' : 'info'}
-					onClick={() => props.handleToothPosition('oclusal')}
-				>
-					Oclusal
-				</Button>
-			</div>
+		<div className={styles.teethFirstRow}>
+			{teeth.map((value, i) => {
+				return (
+					<div className="teethFirstRow_row" key={`value${i}`}>
+						{value.map((number, i) => {
+							return (
+								<div className="row" key={`row${i}`}>
+									{number.map((num, i) => {
+										return (
+											<div className="toothbutton">
+												<span className="toothbuttonNumber">
+													{num.title}
+												</span>
+												<Button
+													variant="outlined"
+													className="palatina"
+												></Button>
+												<Button
+													variant="outlined"
+													className="mesial"
+												></Button>
+												<Button
+													variant="outlined"
+													className="distal"
+												></Button>
+												<Button
+													variant="outlined"
+													className="vestibular"
+												></Button>
+												<Button
+													variant="outlined"
+													className="oclusal"
+												></Button>
+											</div>
+										)
+									})}
+								</div>
+							)
+						})}
+					</div>
+				)
+			})}
 		</div>
 	)
 }
