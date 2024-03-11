a02311a07c50bf5aeb19f657e0a0e9d6ca6cfe6e
Ricardo Alf <rear_recinos@hotmail.com>
2024-03-10 22:04:34 -0600

    Add pagination appointments table

--- src/components/AppointmentsTable.tsx
+++ src/components/AppointmentsTable.tsx
@@ -1,16 +1,36 @@
 import {
+	IconButton,
 	Paper,
 	Table,
 	TableBody,
 	TableCell,
 	TableContainer,
+	TableFooter,
 	TableHead,
+	TablePagination,
 	TableRow,
+	Tooltip,
 } from '@mui/material'
 import useAppointmentState from '@/states/appointmentsState'
+import TablePaginationActions from './TablePaginationActions'
+import { FiCalendar } from 'react-icons/fi'
 
 const AppointmentsTable = () => {
-	const { appointments } = useAppointmentState()
+	const { appointments, setPage, setRowsPerPage, page, rowsPerPage } = useAppointmentState()
+
+	const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - appointments.length) : 0
+
+	const handleChangePage = (_e: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
+		setPage(newPage)
+	}
+
+	const handleChangeRowsPerPage = (
+		event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+	) => {
+		setRowsPerPage(parseInt(event.target.value, 10))
+		setPage(0)
+	}
+
 	return (
 		<TableContainer component={Paper}>
 			<Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
@@ -24,44 +44,53 @@ const AppointmentsTable = () => {
 					</TableRow>
 				</TableHead>
 				<TableBody>
-					{appointments.map(data => (
+					{(rowsPerPage > 0
+						? appointments.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+						: appointments
+					).map(data => (
 						<TableRow key={data.id}>
 							<TableCell>{data.formatDate}</TableCell>
 							<TableCell>{data.treatment}</TableCell>
 							<TableCell align="center">$ {data.cost}</TableCell>
 							<TableCell align="center">{data.doctor}</TableCell>
-							<TableCell align="center"></TableCell>
+							<TableCell align="center">
+								<Tooltip title="Ver cita">
+									<IconButton>
+										<FiCalendar />
+									</IconButton>
+								</Tooltip>
+							</TableCell>
 						</TableRow>
 					))}
+					{emptyRows > 0 && (
+						<TableRow style={{ height: 53 * emptyRows }}>
+							<TableCell colSpan={6} />
+						</TableRow>
+					)}
 				</TableBody>
-				{/* <TableFooter>
-								<TableRow>
-									<TablePagination
-										rowsPerPageOptions={[
-											5,
-											10,
-											25,
-											{ label: 'All', value: -1 },
-										]}
-										colSpan={5}
-										count={allPatients.length}
-										rowsPerPage={rowsPerPage}
-										page={page}
-										labelRowsPerPage="Filas pro página"
-										slotProps={{
-											select: {
-												inputProps: {
-													'aria-label': 'Filas por página',
-												},
-												native: false,
-											},
-										}}
-										onPageChange={handleChangePage}
-										onRowsPerPageChange={handleChangeRowsPerPage}
-										ActionsComponent={TablePaginationActions}
-									/>
-								</TableRow>
-							</TableFooter> */}
+				<TableFooter>
+					<TableRow>
+						<TablePagination
+							rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+							colSpan={5}
+							count={appointments.length}
+							rowsPerPage={rowsPerPage}
+							page={page}
+							labelRowsPerPage="Filas pro página"
+							slotProps={{
+								select: {
+									inputProps: {
+										'aria-label': 'Filas por página',
+									},
+									native: false,
+								},
+							}}
+							onPageChange={handleChangePage}
+							onRowsPerPageChange={handleChangeRowsPerPage}
+							ActionsComponent={TablePaginationActions}
+						/>
+					</TableRow>
+				</TableFooter>
 			</Table>
 		</TableContainer>
 	)
