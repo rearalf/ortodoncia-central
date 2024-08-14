911f876e145c9da9f09428d480cc218822f1ca15
Ricardo Alf <rear_recinos@hotmail.com>
2024-08-13 22:54:19 -0600

    fix: Fix link manual

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
 	return (
 		<footer className="footer">
 			<div className='footer_container'>
-				<a href="manual.pdf" target="_blank" rel="noopener noreferrer" className='manual_link'>
+				<a href="/manual.pdf" target="_blank" rel="noopener noreferrer" className='manual_link'>
 					Manual
 				</a>
 			</div>
