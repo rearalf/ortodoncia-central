fdc3b805ceacfa3d0fc479f8d6748d9dae2ae34e
Ricardo Alf <rear_recinos@hotmail.com>
2024-09-06 21:23:30 -0600

    fix: Fix issues with footer container

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,13 +1,21 @@
+import { Container } from '@mui/material'
 import './styles.css'
 
 const Footer = () => {
 	return (
 		<footer className="footer">
-			<div className='footer_container'>
-				<a href="/manual.pdf" target="_blank" rel="noopener noreferrer" className='manual_link'>
-					Manual
-				</a>
-			</div>
+			<Container maxWidth="xl" className='container_footer'>
+				<div className="footer_container">
+					<a
+						href="/manual.pdf"
+						target="_blank"
+						rel="noopener noreferrer"
+						className="manual_link"
+					>
+						Manual
+					</a>
+				</div>
+			</Container>
 		</footer>
 	)
 }
