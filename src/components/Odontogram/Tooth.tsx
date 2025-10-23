747eefaeddfa19c4b7b2a5787217b92b61121a31
Ricardo Alf <rear_recinos@hotmail.com>
2025-10-23 14:10:58 -0600

    refactor: Standardize formatting and improve readability in TeethForm, Tooth, and functions

--- src/components/Odontogram/Tooth.tsx
+++ src/components/Odontogram/Tooth.tsx
@@ -1,97 +1,131 @@
-import { Button } from '@mui/material'
+import { Button } from "@mui/material";
 
 interface Props {
-	tooth: toothObject
-	quadrant: number
-	hanldeModifyStateTooth: (quadrant: number, tooth: number, position?: toothPosition) => void
+  tooth: toothObject;
+  quadrant: number;
+  hanldeModifyStateTooth: (
+    quadrant: number,
+    tooth: number,
+    position?: toothPosition
+  ) => void;
 }
 
 const Tooth = (props: Props) => (
-	<div className="tooth_button" key={props.tooth.tooth}>
-		<button
-			className={`${'toothState'} ${
-				props.tooth.toothState === ''
-					? ''
-					: props.tooth.toothState === 'extraction'
-					? 'activeExtraction'
-					: 'activeExtracted'
-			}`}
-			type="button"
-			onClick={() => props.hanldeModifyStateTooth(props.quadrant, props.tooth.tooth)}
-		>
-			X
-		</button>
+  <div className="tooth_button" key={props.tooth.tooth}>
+    <button
+      className={`${"toothState"} ${
+        props.tooth.toothState === ""
+          ? ""
+          : props.tooth.toothState === "extraction"
+          ? "activeExtraction"
+          : "activeExtracted"
+      }`}
+      type="button"
+      onClick={() =>
+        props.hanldeModifyStateTooth(props.quadrant, props.tooth.tooth)
+      }
+    >
+      X
+    </button>
 
-		<button
-			className={`abutment_tooth_state ${props.tooth.abutmentTooth ? 'abutment_tooth' : ''} ${
-				props.tooth.falseTooth ? 'false_tooth' : ''
-			}`}
-			onClick={() => props.hanldeModifyStateTooth(props.quadrant, props.tooth.tooth)}
-		></button>
+    <button
+      className={`abutment_tooth_state ${
+        props.tooth.abutmentTooth ? "abutment_tooth" : ""
+      } ${props.tooth.falseTooth ? "false_tooth" : ""}`}
+      onClick={() =>
+        props.hanldeModifyStateTooth(props.quadrant, props.tooth.tooth)
+      }
+    ></button>
 
-		<button
-			className={`pit_fissure_sealant ${
-				props.tooth.pitFissureSealant === 1
-					? 'sealant_tooth_red'
-					: props.tooth.pitFissureSealant === 2
-					? 'sealant_tooth_blue'
-					: ''
-			}`}
-			onClick={() => props.hanldeModifyStateTooth(props.quadrant, props.tooth.tooth)}
-		></button>
+    <button
+      className={`pit_fissure_sealant ${
+        props.tooth.pitFissureSealant === 1
+          ? "sealant_tooth_blue"
+          : props.tooth.pitFissureSealant === 2
+          ? "sealant_tooth_red"
+          : ""
+      }`}
+      onClick={() =>
+        props.hanldeModifyStateTooth(props.quadrant, props.tooth.tooth)
+      }
+    ></button>
 
-		<button
-			className={`${'toothButtonNumber'} ${
-				props.tooth.oclusal !== '' ? 'toothButtonNumberOver' : ''
-			}`}
-			type="button"
-			onClick={() =>
-				props.hanldeModifyStateTooth(props.quadrant, props.tooth.tooth, 'oclusal')
-			}
-		>
-			{(props.quadrant, props.tooth.tooth)}
-		</button>
-		<Button
-			variant={props.tooth.palatina === '' ? 'outlined' : 'contained'}
-			className="palatina"
-			color={props.tooth.palatina === 'decay' ? 'error' : 'info'}
-			onClick={() =>
-				props.hanldeModifyStateTooth(props.quadrant, props.tooth.tooth, 'palatina')
-			}
-		></Button>
-		<Button
-			variant={props.tooth.mesial === '' ? 'outlined' : 'contained'}
-			className="mesial"
-			color={props.tooth.mesial === 'decay' ? 'error' : 'info'}
-			onClick={() =>
-				props.hanldeModifyStateTooth(props.quadrant, props.tooth.tooth, 'mesial')
-			}
-		></Button>
-		<Button
-			variant={props.tooth.distal === '' ? 'outlined' : 'contained'}
-			className="distal"
-			color={props.tooth.distal === 'decay' ? 'error' : 'info'}
-			onClick={() =>
-				props.hanldeModifyStateTooth(props.quadrant, props.tooth.tooth, 'distal')
-			}
-		></Button>
-		<Button
-			variant={props.tooth.vestibular === '' ? 'outlined' : 'contained'}
-			className="vestibular"
-			color={props.tooth.vestibular === 'decay' ? 'error' : 'info'}
-			onClick={() =>
-				props.hanldeModifyStateTooth(props.quadrant, props.tooth.tooth, 'vestibular')
-			}
-		></Button>
-		<Button
-			variant={props.tooth.oclusal === '' ? 'outlined' : 'contained'}
-			className="oclusal"
-			color={props.tooth.oclusal === 'decay' ? 'error' : 'info'}
-			onClick={() =>
-				props.hanldeModifyStateTooth(props.quadrant, props.tooth.tooth, 'oclusal')
-			}
-		></Button>
-	</div>
-)
+    <button
+      className={`${"toothButtonNumber"} ${
+        props.tooth.oclusal !== "" ? "toothButtonNumberOver" : ""
+      }`}
+      type="button"
+      onClick={() =>
+        props.hanldeModifyStateTooth(
+          props.quadrant,
+          props.tooth.tooth,
+          "oclusal"
+        )
+      }
+    >
+      {(props.quadrant, props.tooth.tooth)}
+    </button>
+    <Button
+      variant={props.tooth.palatina === "" ? "outlined" : "contained"}
+      className="palatina"
+      color={props.tooth.palatina === "decay" ? "error" : "info"}
+      onClick={() =>
+        props.hanldeModifyStateTooth(
+          props.quadrant,
+          props.tooth.tooth,
+          "palatina"
+        )
+      }
+    ></Button>
+    <Button
+      variant={props.tooth.mesial === "" ? "outlined" : "contained"}
+      className="mesial"
+      color={props.tooth.mesial === "decay" ? "error" : "info"}
+      onClick={() =>
+        props.hanldeModifyStateTooth(
+          props.quadrant,
+          props.tooth.tooth,
+          "mesial"
+        )
+      }
+    ></Button>
+    <Button
+      variant={props.tooth.distal === "" ? "outlined" : "contained"}
+      className="distal"
+      color={props.tooth.distal === "decay" ? "error" : "info"}
+      onClick={() =>
+        props.hanldeModifyStateTooth(
+          props.quadrant,
+          props.tooth.tooth,
+          "distal"
+        )
+      }
+    ></Button>
+    <Button
+      variant={props.tooth.vestibular === "" ? "outlined" : "contained"}
+      className="vestibular"
+      color={props.tooth.vestibular === "decay" ? "error" : "info"}
+      onClick={() =>
+        props.hanldeModifyStateTooth(
+          props.quadrant,
+          props.tooth.tooth,
+          "vestibular"
+        )
+      }
+    ></Button>
+    <Button
+      variant={props.tooth.oclusal === "" ? "outlined" : "contained"}
+      className="oclusal"
+      color={props.tooth.oclusal === "decay" ? "error" : "info"}
+      onClick={() =>
+        props.hanldeModifyStateTooth(
+          props.quadrant,
+          props.tooth.tooth,
+          "oclusal"
+        )
+      }
+    ></Button>
+  </div>
+);
 
-export default Tooth
+export default Tooth;
--- src/components/Odontogram/functions.ts
+++ src/components/Odontogram/functions.ts
@@ -1,160 +1,179 @@
 export function modifyPositionStatus(
-	quadrant: number,
-	tooth: number,
-	teethList: Odontogram,
-	positionState: toothPositionStateType,
-	position: toothPosition,
-	setHandleState: setAlertType,
+  quadrant: number,
+  tooth: number,
+  teethList: Odontogram,
+  positionState: toothPositionStateType,
+  position: toothPosition,
+  setHandleState: setAlertType
 ) {
-	const updatedTeethList = { ...teethList }
-	if (quadrant < 5) {
-		updatedTeethList.permanent[quadrant].map(toothObj => {
-			if (toothObj.falseTooth || toothObj.abutmentTooth) {
-				setHandleState({
-					severity: 'warning',
-					variant: 'filled',
-					show: true,
-					text: 'Debe deshacer el puente antes de cambiar todo.',
-				})
-				return
-			}
+  const updatedTeethList = { ...teethList };
+  if (quadrant < 5) {
+    updatedTeethList.permanent[quadrant].map((toothObj) => {
+      if (toothObj.falseTooth || toothObj.abutmentTooth) {
+        setHandleState({
+          severity: "warning",
+          variant: "filled",
+          show: true,
+          text: "Debe deshacer el puente antes de cambiar todo.",
+        });
+        return;
+      }
 
-			if (toothObj.tooth === tooth) {
-				toothObj[position] = positionState === 'disable' ? '' : positionState
-				toothObj.toothState = ''
-			}
-		})
-	} else {
-		updatedTeethList.temporary[quadrant].map(toothObj => {
-			if (toothObj.tooth === tooth) {
-				toothObj[position] = positionState === 'disable' ? '' : positionState
-				toothObj.toothState = ''
-			}
-		})
-	}
-	return updatedTeethList
+      if (toothObj.tooth === tooth) {
+        toothObj[position] = positionState === "disable" ? "" : positionState;
+        toothObj.toothState = "";
+      }
+    });
+  } else {
+    updatedTeethList.temporary[quadrant].map((toothObj) => {
+      if (toothObj.tooth === tooth) {
+        toothObj[position] = positionState === "disable" ? "" : positionState;
+        toothObj.toothState = "";
+      }
+    });
+  }
+  return updatedTeethList;
 }
 
 export function modifyExtractionStatus(
-	tooth: number,
-	quadrant: number,
-	toothState: toothStateType,
-	teethList: Odontogram,
-	setHandleState: setAlertType,
+  tooth: number,
+  quadrant: number,
+  toothState: toothStateType,
+  teethList: Odontogram,
+  setHandleState: setAlertType
 ) {
-	const updatedTeethList = { ...teethList }
-	if (quadrant < 5) {
-		updatedTeethList.permanent[quadrant].map(toothObj => {
-			if (toothObj.falseTooth || toothObj.abutmentTooth) {
-				setHandleState({
-					severity: 'warning',
-					variant: 'filled',
-					show: true,
-					text: 'Debe deshacer el puente antes de cambiar todo.',
-				})
-				return
-			}
+  const updatedTeethList = { ...teethList };
+  if (quadrant < 5) {
+    updatedTeethList.permanent[quadrant].map((toothObj) => {
+      if (toothObj.falseTooth || toothObj.abutmentTooth) {
+        setHandleState({
+          severity: "warning",
+          variant: "filled",
+          show: true,
+          text: "Debe deshacer el puente antes de cambiar todo.",
+        });
+        return;
+      }
 
-			if (toothObj.tooth === tooth) {
-				toothObj.toothState = toothState === 'disable' ? '' : toothState
-			}
-		})
-	} else {
-		updatedTeethList.temporary[quadrant].map(toothObj => {
-			if (toothObj.tooth === tooth) {
-				toothObj.toothState = toothState === 'disable' ? '' : toothState
-			}
-		})
-	}
-	return updatedTeethList
+      if (toothObj.tooth === tooth) {
+        toothObj.toothState = toothState === "disable" ? "" : toothState;
+      }
+    });
+  } else {
+    updatedTeethList.temporary[quadrant].map((toothObj) => {
+      if (toothObj.tooth === tooth) {
+        toothObj.toothState = toothState === "disable" ? "" : toothState;
+      }
+    });
+  }
+  return updatedTeethList;
 }
 
 export function modifyFixedPartialBridge(
-	quadrant: number,
-	tooth: number,
-	teethList: Odontogram,
-	abutmentToothState: abutmentToothStateType,
-	setHandleState: setAlertType,
+  quadrant: number,
+  tooth: number,
+  teethList: Odontogram,
+  abutmentToothState: abutmentToothStateType,
+  setHandleState: setAlertType
 ) {
-	const updatedTeethList = { ...teethList }
+  const updatedTeethList = { ...teethList };
 
-	if (quadrant >= 5) {
-		setHandleState({
-			severity: 'error',
-			variant: 'filled',
-			show: true,
-			text: 'No se hacen puentes en este cuadrante.',
-		})
-		return
-	}
+  if (quadrant >= 5) {
+    setHandleState({
+      severity: "error",
+      variant: "filled",
+      show: true,
+      text: "No se hacen puentes en este cuadrante.",
+    });
+    return;
+  }
 
-	const abutments = updatedTeethList.permanent[quadrant].filter(t => t.tooth === tooth)[0]
+  const abutments = updatedTeethList.permanent[quadrant].filter(
+    (t) => t.tooth === tooth
+  )[0];
 
-	if (abutmentToothState === true && abutments.toothState !== '') {
-		setHandleState({
-			severity: 'warning',
-			variant: 'filled',
-			show: true,
-			text: 'Este diente ya ha sido modificado.',
-		})
-		return
-	}
+  if (abutmentToothState === true && abutments.toothState !== "") {
+    setHandleState({
+      severity: "warning",
+      variant: "filled",
+      show: true,
+      text: "Este diente ya ha sido modificado.",
+    });
+    return;
+  }
 
-	if (abutmentToothState === 'disable') {
-		updatedTeethList.permanent[quadrant].map(toothObj => {
-			if (toothObj.tooth === tooth) {
-				toothObj.falseTooth = false
-				toothObj.abutmentTooth = false
-			}
-		})
-	} else if (abutmentToothState === 'falseTooth') {
-		updatedTeethList.permanent[quadrant].map(toothObj => {
-			if (toothObj.tooth === tooth) {
-				toothObj.falseTooth = true
-			}
-		})
-	} else {
-		updatedTeethList.permanent[quadrant].map(toothObj => {
-			if (toothObj.tooth === tooth) {
-				toothObj.abutmentTooth = true
-			}
-		})
-	}
+  if (abutmentToothState === "disable") {
+    updatedTeethList.permanent[quadrant].map((toothObj) => {
+      if (toothObj.tooth === tooth) {
+        toothObj.falseTooth = false;
+        toothObj.abutmentTooth = false;
+      }
+    });
+  } else if (abutmentToothState === "falseTooth") {
+    updatedTeethList.permanent[quadrant].map((toothObj) => {
+      if (toothObj.tooth === tooth) {
+        toothObj.falseTooth = true;
+      }
+    });
+  } else {
+    updatedTeethList.permanent[quadrant].map((toothObj) => {
+      if (toothObj.tooth === tooth) {
+        toothObj.abutmentTooth = true;
+      }
+    });
+  }
 
-	return updatedTeethList
+  return updatedTeethList;
 }
 
 export function modifyPitFissuereSealant(
-	tooth: number,
-	quadrant: number,
-	teethList: Odontogram,
-	pitFissureSealant: pitFissureSealantType,
-	setHandleState: setAlertType,
+  tooth: number,
+  quadrant: number,
+  teethList: Odontogram,
+  pitFissureSealant: pitFissureSealantType,
+  setHandleState: setAlertType
 ) {
-	const updatedTeethList = { ...teethList }
-	if (quadrant < 5) {
-		updatedTeethList.permanent[quadrant].map(toothObj => {
-			if (toothObj.tooth === tooth) {
-				if (toothObj.abutmentTooth || toothObj.falseTooth || toothObj.toothState !== '') {
-					setHandleState({
-						severity: 'warning',
-						variant: 'filled',
-						show: true,
-						text: 'No puede marcar este diente',
-					})
-					return
-				}
-				toothObj.pitFissureSealant = pitFissureSealant
-				return
-			}
-		})
-	} else {
-		setHandleState({
-			severity: 'warning',
-			variant: 'filled',
-			show: true,
-			text: 'No se deben de marcar sellantes en este cuadrante.',
-		})
-	}
-	return updatedTeethList
+  const updatedTeethList = { ...teethList };
+  if (quadrant < 5) {
+    updatedTeethList.permanent[quadrant].map((toothObj) => {
+      if (toothObj.tooth === tooth) {
+        if (
+          toothObj.abutmentTooth ||
+          toothObj.falseTooth ||
+          toothObj.toothState !== ""
+        ) {
+          setHandleState({
+            severity: "warning",
+            variant: "filled",
+            show: true,
+            text: "No puede marcar este diente",
+          });
+          return;
+        }
+        toothObj.pitFissureSealant = pitFissureSealant;
+        return;
+      }
+    });
+  } else {
+    updatedTeethList.temporary[quadrant].map((toothObj) => {
+      if (toothObj.tooth === tooth) {
+        if (
+          toothObj.abutmentTooth ||
+          toothObj.falseTooth ||
+          toothObj.toothState !== ""
+        ) {
+          setHandleState({
+            severity: "warning",
+            variant: "filled",
+            show: true,
+            text: "No puede marcar este diente",
+          });
+          return;
+        }
+        toothObj.pitFissureSealant = pitFissureSealant;
+        return;
+      }
+    });
+  }
+  return updatedTeethList;
 }
